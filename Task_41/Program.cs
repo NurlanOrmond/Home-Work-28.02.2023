75689493bd46c59fa1363c131670a7616d6d7ac8
Nurlan Ormond <sunshineforester@gmail.com>
2023-03-01 11:40:22 +0300

    Home work done

---
+++ Task_41/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("\n\nHow many numbers are you going to enter?");
+int n = int.Parse(Console.ReadLine());
+
+int [] CreateArray (int b)
+{
+    int [] arr = new int [n];
+
+    for (int i = 0; i < n; i++)
+    {
+        System.Console.Write($"Enter your {i+1} number: ");
+        arr[i] = int.Parse(Console.ReadLine());
+        
+    }
+    return arr;
+}
+
+void ShowArray (int []a)
+{
+    int cnt=0;
+    foreach (var item in a)
+    {
+        if(item>0) cnt++;
+    }
+    System.Console.WriteLine("***************************************");
+    System.Console.WriteLine($"Quantity of your positive numbers is: {cnt}\n\n");
+}
+
+int [] a = CreateArray(n);
+
+ShowArray(a);
---
+++ Task_43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("\n\nDefine cross point of two Linear Functions \n\n"+"****************\n" + 
+"y = k1 * x + b1 \n"+"y = k2 * x + b2\n"+"****************");
+
+
+
+    System.Console.Write("\nEnter Slope (k1) for the 1st Linear function: ");
+    List <double> consts = new List<double>();
+    double k1 = int.Parse(Console.ReadLine());
+    consts.Add (k1);
+    System.Console.Write("\nEnter Intercept (b1) for the 1st Linear function: ");
+    double b1 =int.Parse(Console.ReadLine());
+    consts.Add (b1);
+
+    System.Console.Write("\nEnter Slope (k2) for the 2nd Linear function: ");
+    double k2 = int.Parse(Console.ReadLine());
+    consts.Add (k2);
+    System.Console.Write("\nEnter Intercept (b2) for the 2nd Linear function: ");
+    double b2 = int.Parse(Console.ReadLine());
+    consts.Add (b2); 
+
+    
+
+      
+void CrossPoint (List <double> list)
+{
+    double k1 = list[0];
+    double b1 = list[1];
+    double k2 = list[2];
+    double b2 = list[3];
+
+double x=(b2-b1)/(k1-k2);
+double y=k2*x+b2;
+
+System.Console.WriteLine("****************************************************");
+System.Console.WriteLine($"\n\nCross point of two lines at X={x} and Y={y}\n\n");
+}
+
+
+CrossPoint (consts);
